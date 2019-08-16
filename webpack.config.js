95415263bf5c3c6d71b78c1b2c71d5028749ba28
brownnrl <brownnrl@gmail.com>
2019-08-15 20:55:10 -0400

    Add webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      // All files with a '.ts' extension will be handled by 'awesome-typescript-loader'.
+      { test: /\.ts$/, loader: "awesome-typescript-loader" },
+
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js', '.json' ]
+  },
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+  // When importing a module whose path matches one of the following, just
+  // assume a corresponding global variable exists and use that instead.
+  // This is important because it allows us to avoid bundling all of our
+  // dependencies, which allows browsers to cache those libraries between builds.
+  externals: {
+      "paper": "paper"
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'var',
+    library: 'geomlib'
+  },
+  mode: 'development'
+};
