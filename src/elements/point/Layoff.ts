be90934b793e6d018217700be6d9b43574adfad5
brownnrl <brownnrl@gmail.com>
2019-09-06 22:28:42 -0400

    Start to Layoff

---
+++ src/elements/point/Layoff.ts
@@ -0,0 +1,30 @@
+/*----------------------------------------------------------------------+
+|    Title:	Layoff.ts                                                   |
+|    A port of the software Geometry Applet by                          |
+|    Author:    David E. Joyce                                          |
+|        Department of Mathematics and Computer Science                 |
+|        Clark University                                               |
+|        Worcester, MA 01610-1477                                       |
+|        U.S.A.                                                         |
+|                                                                       |
+|        http://aleph0.clarku.edu/~djoyce/home.html                     |
+|        djoyce@clarku.edu                                              |
+|                                                                       |
+|    Date:    February, 1996.   Version 2.0.0 May, 1997.                |
+|    TypeScript Port: 2019, Nelson Brown, brownnrl@gmail.com            |
+|                           https://www.nelsonbrown.net/                |
++----------------------------------------------------------------------*/
+
+import {PointElement} from "./PointElement";
+
+export class Layoff extends PointElement {
+
+    protected _A : PointElement;
+    protected _B : PointElement;
+    protected _C : PointElement;
+    protected _D : PointElement;
+
+
+    // TODO: Pick up here.
+
+}
