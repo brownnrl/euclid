42702ea9fb1463e18a192a6b6491063910b11f20
brownnrl <brownnrl@gmail.com>
2019-09-06 22:25:40 -0400

    Add headers

--- src/elements/point/FixedPoint.ts
+++ src/elements/point/FixedPoint.ts
@@ -1,3 +1,19 @@
+/*----------------------------------------------------------------------+
+|    Title:	FixedPoint.ts                                               |
+|    A port of the software Geometry Applet by                          |
+|    Author:    David E. Joyce                                          |
+|        Department of Mathematics and Computer Science                 |
+|        Clark University                                               |
+|        Worcester, MA 01610-1477                                       |
+|        U.S.A.                                                         |
+|                                                                       |
+|        http://aleph0.clarku.edu/~djoyce/home.html                     |
+|        djoyce@clarku.edu                                              |
+|                                                                       |
+|    Date:    February, 1996.   Version 2.0.0 May, 1997.                |
+|    TypeScript Port: 2019, Nelson Brown, brownnrl@gmail.com            |
+|                           https://www.nelsonbrown.net/                |
++----------------------------------------------------------------------*/
 import {PointElement} from "./PointElement";
 import {PlaneElement} from "../plane/PlaneElement";
 
